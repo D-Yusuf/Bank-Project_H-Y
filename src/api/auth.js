9d4691774ae437a6cc301f3c145ae4004f267eec
Yusuf Alnasiri <alnasiriyusuf@gmail.com>
2024-09-03 19:20:06 +0300

    updated

---
+++ src/api/auth.js
@@ -0,0 +1,25 @@
+import instance from ".";
+function register(){
+//post
+}
+function login(){
+//post (return posted email&password user and give token)
+}
+function getMyInfo(){
+//get
+}
+function getAllUsers(){
+//get
+}
+function deposit(amount){
+//put (update)
+}
+function withdraw(amount){
+//put 
+}
+function transfer(amount, userName){
+//put
+}
+function getUserInfo(){
+// will use useQuery when path is 
+}
